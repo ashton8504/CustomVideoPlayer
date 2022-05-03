e9b62934707c98544ce183b854783c52334bf5bb
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-05-03 16:07:26 -0400

    saving code

--- scripts.js
+++ scripts.js
@@ -53,3 +53,4 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+   
